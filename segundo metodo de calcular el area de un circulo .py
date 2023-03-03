0972a7bf0461fc3862d066679e24750c9f5041e4
gimenavargas <1471366@senati.pe>
2023-03-03 10:41:47 -0500

    area del circulo usando math

---
+++ segundo metodo de calcular el area de un circulo .py
@@ -0,0 +1,8 @@
+#DESARROLLAR UN ALGORITMO DE PYTHON QUE CALCULE EL AREA DEL CIRCULO
+#USANDO MATH
+import math 
+
+print("introduzca el valor del radio")
+radio = float (input())
+area = math.pi * radio ** 2
+print("El area es:", area)
